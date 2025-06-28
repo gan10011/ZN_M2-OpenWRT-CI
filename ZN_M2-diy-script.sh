4130d93709a64f57cf0149e2653b91ed8b8a741b
gan10011 <329377128@qq.com>
2025-06-29 07:41:57 +0800

    Update ZN_M2-diy-script.sh

--- ZN_M2-diy-script.sh
+++ ZN_M2-diy-script.sh
@@ -10,14 +10,14 @@ sed -i 's/192.168.100.1/192.168.1.1/g' package/base-files/files/bin/config_gener
  sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
-# rm -rf feeds/packages/net/mosdns
-# rm -rf feeds/packages/net/msd_lite
-# rm -rf feeds/packages/net/smartdns
-# rm -rf feeds/luci/themes/luci-theme-argon
-# rm -rf feeds/luci/themes/luci-theme-netgear
-# rm -rf feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/luci/applications/luci-app-netdata
-# rm -rf feeds/luci/applications/luci-app-serverchan
+ rm -rf feeds/packages/net/mosdns
+ rm -rf feeds/packages/net/msd_lite
+ rm -rf feeds/packages/net/smartdns
+ rm -rf feeds/luci/themes/luci-theme-argon
+ rm -rf feeds/luci/themes/luci-theme-netgear
+ rm -rf feeds/luci/applications/luci-app-mosdns
+ rm -rf feeds/luci/applications/luci-app-netdata
+ rm -rf feeds/luci/applications/luci-app-serverchan
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -30,17 +30,17 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
-git_sparse_clone master https://github.com/sundaqiang/openwrt-packages luci-app-wolplus
-git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki nikki
-git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki luci-app-nikki
+#git_sparse_clone master https://github.com/sundaqiang/openwrt-packages luci-app-wolplus
+#git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki nikki
+#git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki luci-app-nikki
 # 添加nikki
 # add feed
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+#echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 # update & install feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 # make package
-make package/luci-app-nikki/compile
+#make package/luci-app-nikki/compile
 
 # git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
@@ -57,7 +57,7 @@ make package/luci-app-nikki/compile
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+#git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
 # git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
@@ -110,9 +110,9 @@ git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclas
 # sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 # 修改版本为编译日期
-# date_version=$(date +"%y.%m.%d")
-# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-# sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+ date_version=$(date +"%y.%m.%d")
+ orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+ sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修复 hostapd 报错
 # cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
